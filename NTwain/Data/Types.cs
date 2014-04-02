6f4e70ae7b70f3052bb5ef3fb0ce35c8f4872288
soukoku <yinchun.wang@gmail.com>
2014-04-02 19:13:15 -0400

    Removed header text.

--- NTwain/Data/Types.cs
+++ NTwain/Data/Types.cs
@@ -1,32 +1,12 @@
-﻿// The MIT License (MIT)
-// Copyright (c) 2013 Yin-Chun Wang
-//
-// Permission is hereby granted, free of charge, to any person obtaining a 
-// copy of this software and associated documentation files (the "Software"), 
-// to deal in the Software without restriction, including without limitation 
-// the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-// and/or sell copies of the Software, and to permit persons to whom the 
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included 
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF 
-// OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-using System;
-using System.Runtime.InteropServices;
-// This file contains all the structs defined in the twain.h file.
+﻿// This file contains all the structs defined in the twain.h file.
 
 // The TWAIN numeric types are mapped with "using"
 // to aid in mapping against the twin.h file (copy/paste!).
 // It also makes it easy to change all the types that 
 // uses it if I made a mistake in the mapped value type.
 // Consumers will not see those names.
+using System;
+using System.Runtime.InteropServices;
 
 using TW_BOOL = System.UInt16;
 // use HandleRef instead?
