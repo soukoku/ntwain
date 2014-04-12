78d0a94bd2c074938c7df8477f28e5082ba38ef1
soukoku <yinchun.wang@gmail.com>
2014-04-12 11:12:18 -0400

    How do I keep missing new file?

---
+++ NTwain/TransferErrorEventArgs.cs
@@ -0,0 +1,39 @@
+﻿using NTwain.Data;
+using NTwain.Values;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NTwain
+{
+    /// <summary>
+    /// Contains TWAIN codes and source status when an error is encountered during transfer.
+    /// </summary>
+    public class TransferErrorEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets the return code.
+        /// </summary>
+        /// <value>
+        /// The return code.
+        /// </value>
+        public ReturnCode ReturnCode { get; internal set; }
+
+        /// <summary>
+        /// Gets the source status.
+        /// </summary>
+        /// <value>
+        /// The source status.
+        /// </value>
+        public TWStatus SourceStatus { get; internal set; }
+
+        /// <summary>
+        /// Gets the exception if the error is from some exception.
+        /// </summary>
+        /// <value>
+        /// The exception.
+        /// </value>
+        public Exception Exception { get; internal set; }
+    }
+}
