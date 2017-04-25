f59a9f725f8ea1cc711ebb9d2aa6d58a6413ed62
Eugene Wang <yinchun.wang@outlook.com>
2017-04-24 23:23:28 -0400

    version bump for 3.4.2

--- src/NTwain/Properties/VersionInfo.cs
+++ src/NTwain/Properties/VersionInfo.cs
@@ -23,7 +23,7 @@ namespace NTwain
         /// <summary>
         /// The build release version number.
         /// </summary>
-        public const string Build = "3.4.1"; // change this for each nuget release
+        public const string Build = "3.4.2"; // change this for each nuget release
 
 
     }
