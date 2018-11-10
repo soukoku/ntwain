9e2051045511bf5057fc4a55ba3f8f6a289779c2
Eugene Wang <yinchun.wang@gmail.com>
2018-11-10 16:34:57 -0500

    Added sample netcore console app placeholder.

---
+++ samples/NetCoreConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using NTwain;
+using System;
+using System.Reflection;
+
+namespace NetCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new TwainConfigurationBuilder()
+                .DefineApp(Assembly.GetExecutingAssembly())
+                .Build();
+            using (var session = new TwainSession(config))
+            {
+
+            }
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
