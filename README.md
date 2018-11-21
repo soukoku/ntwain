720524d0b443fcd4222e9d76d4756643078bf8a9
Eugene Wang <yinchun.wang@gmail.com>
2018-11-20 21:23:34 -0500

    Updated readme and c++ ref file.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TWAIN Application-Library
+# TWAIN dotnet library
 
 NOTE: THIS IS V4 DEV AND DOESN'T WORK YET.
 USE V3 BRANCH FOR WORKING VERSION.
@@ -10,9 +10,9 @@ This is a library created to make working with
 V4 of this lib has these goals:
 
 * Targets latest TWAIN version (2.4 as of this writing)
-* Supports all the TWAIN functions in the spec
-* Works with both 32 and 64 bit data sources
-* Works on non-Windows platforms using dotnet core or mono (maybe)
+* Supports all the TWAIN functions in the spec (directly or through dotnet wrapper)
+* Works with both 32 or 64 bit data sources, from 32 or 64 bit apps.
+* Works on non-Windows platforms using dotnet core
 
 ## Using the lib
 
--- size-test/main.cpp
+++ size-test/main.cpp
@@ -1,5 +1,6 @@
 // code block to verify size of things on different platforms and archtectures
 
+#include <iostream>
 #include "twain.h"
 
 int main()
@@ -8,24 +9,24 @@ int main()
 	bool is64 = sizeof(void*) == 8;
 	std::cout << (is32 ? "32bit= " : "") << (is64 ? "64bit" : "") << std::endl;
 	std::cout << std::endl;
-	std::cout << "TW_STR32  = " << sizeof TW_STR32 << std::endl;
-	std::cout << "TW_STR64  = " << sizeof TW_STR64 << std::endl;
-	std::cout << "TW_STR128 = " << sizeof TW_STR128 << std::endl;
-	std::cout << "TW_STR255 = " << sizeof TW_STR255 << std::endl;
+	std::cout << "TW_STR32  = " << sizeof(TW_STR32) << std::endl;
+	std::cout << "TW_STR64  = " << sizeof(TW_STR64) << std::endl;
+	std::cout << "TW_STR128 = " << sizeof(TW_STR128) << std::endl;
+	std::cout << "TW_STR255 = " << sizeof(TW_STR255) << std::endl;
 	std::cout << std::endl;
-	std::cout << "TW_INT8   = " << sizeof TW_INT8 << std::endl;
-	std::cout << "TW_INT16  = " << sizeof TW_INT16 << std::endl;
-	std::cout << "TW_INT32  = " << sizeof TW_INT32 << std::endl;
-	std::cout << "TW_UINT8  = " << sizeof TW_UINT8 << std::endl;
-	std::cout << "TW_UINT16 = " << sizeof TW_UINT16 << std::endl;
-	std::cout << "TW_UINT32 = " << sizeof TW_UINT32 << std::endl;
-	std::cout << "TW_BOOL   = " << sizeof TW_BOOL << std::endl;
+	std::cout << "TW_INT8   = " << sizeof(TW_INT8) << std::endl;
+	std::cout << "TW_INT16  = " << sizeof(TW_INT16) << std::endl;
+	std::cout << "TW_INT32  = " << sizeof(TW_INT32) << std::endl;
+	std::cout << "TW_UINT8  = " << sizeof(TW_UINT8) << std::endl;
+	std::cout << "TW_UINT16 = " << sizeof(TW_UINT16) << std::endl;
+	std::cout << "TW_UINT32 = " << sizeof(TW_UINT32) << std::endl;
+	std::cout << "TW_BOOL   = " << sizeof(TW_BOOL) << std::endl;
 	std::cout << std::endl;
-	std::cout << "TW_HANDLE = " << sizeof TW_HANDLE << std::endl;
-	std::cout << "TW_MEMREF = " << sizeof TW_MEMREF << std::endl;
-	std::cout << "TW_UINTPTR= " << sizeof TW_UINTPTR << std::endl;
+	std::cout << "TW_HANDLE = " << sizeof(TW_HANDLE) << std::endl;
+	std::cout << "TW_MEMREF = " << sizeof(TW_MEMREF) << std::endl;
+	std::cout << "TW_UINTPTR= " << sizeof(TW_UINTPTR) << std::endl;
 	std::cout << std::endl;
-	std::cout << "TW_IDENTITY = " << sizeof TW_IDENTITY << std::endl;
+	std::cout << "TW_IDENTITY = " << sizeof(TW_IDENTITY) << std::endl;
 	int test;
 	std::cin >> test;
 }
