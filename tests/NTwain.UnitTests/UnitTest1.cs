2fb153fa59caf1321ec406bec3fc5d137cf6d241
Eugene Wang <yinchun.wang@gmail.com>
2018-11-25 22:03:47 -0500

    Added unit test project placeholder.

---
+++ tests/NTwain.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NTwain.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail("Placeholder for now.");
+        }
+    }
+}
