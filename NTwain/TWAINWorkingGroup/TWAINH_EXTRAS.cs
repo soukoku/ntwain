5f443d59e1b4108220b1c250fec495ab0798b0e0
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2021-04-21 22:42:56 -0400

    Add implicit string conversions.

--- NTwain/TWAINWorkingGroup/TWAINH_EXTRAS.cs
+++ NTwain/TWAINWorkingGroup/TWAINH_EXTRAS.cs
@@ -10,34 +10,67 @@ namespace TWAINWorkingGroup
 
     partial struct TW_STR32
     {
+        public TW_STR32(string value) : this()
+        {
+            Set(value);
+        }
+
         public override string ToString()
         {
             return Get();
         }
+
+        public static implicit operator string(TW_STR32 value) => value.ToString();
+        public static explicit operator TW_STR32(string value) => new TW_STR32(value);
+
     }
 
     partial struct TW_STR64
     {
+        public TW_STR64(string value) : this()
+        {
+            Set(value);
+        }
+
         public override string ToString()
         {
             return Get();
         }
+
+        public static implicit operator string(TW_STR64 value) => value.ToString();
+        public static explicit operator TW_STR64(string value) => new TW_STR64(value);
     }
 
     partial struct TW_STR128
     {
+        public TW_STR128(string value) : this()
+        {
+            Set(value);
+        }
+
         public override string ToString()
         {
             return Get();
         }
+
+        public static implicit operator string(TW_STR128 value) => value.ToString();
+        public static explicit operator TW_STR128(string value) => new TW_STR128(value);
     }
 
     partial struct TW_STR255
     {
+        public TW_STR255(string value) : this()
+        {
+            Set(value);
+        }
+
         public override string ToString()
         {
             return Get();
         }
+
+        public static implicit operator string(TW_STR255 value) => value.ToString();
+        public static explicit operator TW_STR255(string value) => new TW_STR255(value);
     }
 
     partial struct TW_IDENTITY
