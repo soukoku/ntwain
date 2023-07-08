bf658d142a9aa397c0e92f059370752b1aa2b5af
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2023-07-08 11:08:27 -0400

    Started experiment with unpackaged winui3 project.

---
+++ samples/WinUI3/NoOpStream.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace WinUI3
+{
+  internal class NoOpStream : Stream
+  {
+    public override bool CanRead => false;
+
+    public override bool CanSeek => false;
+
+    public override bool CanWrite => true;
+
+    public override long Length => 0;
+
+    public override long Position { get => 0; set { } }
+
+    public override void Flush()
+    {
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void SetLength(long value)
+    {
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+    }
+  }
+}
