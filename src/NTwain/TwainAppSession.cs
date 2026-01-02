b9988b2f1326323bbf213005a3af8ff5a44b31ea
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2026-01-01 23:17:52 -0500

    Fixed incorrect logger getter.

--- src/NTwain/TwainAppSession.cs
+++ src/NTwain/TwainAppSession.cs
@@ -46,7 +46,7 @@ namespace NTwain
 
         public ILogger Logger
         {
-            get { return _logger = NullLogger.Instance; }
+            get { return _logger; }
             set { _logger = value ?? NullLogger.Instance; }
         }
 
