7731063a9d9bfd3de5d7f581242a7e7b552494f9
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2026-01-08 22:09:49 -0500

    Update with latest v4 beta.

--- samples/WinUI3/App.xaml.cs
+++ samples/WinUI3/App.xaml.cs
@@ -19,20 +19,21 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace WinUI3
+namespace WinUI3;
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : Application
 {
-  /// <summary>
-  /// Provides application-specific behavior to supplement the default Application class.
-  /// </summary>
-  public partial class App : Application
-  {
+    private Window? _window;
+
     /// <summary>
     /// Initializes the singleton application object.  This is the first line of authored code
     /// executed, and as such is the logical equivalent of main() or WinMain().
     /// </summary>
     public App()
     {
-      this.InitializeComponent();
+        InitializeComponent();
     }
 
     /// <summary>
@@ -41,10 +42,7 @@ namespace WinUI3
     /// <param name="args">Details about the launch request and process.</param>
     protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
     {
-      m_window = new MainWindow();
-      m_window.Activate();
+        _window = new MainWindow();
+        _window.Activate();
     }
-
-    private Window m_window;
-  }
 }
